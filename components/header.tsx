53cd0b55c7409baa685e54cd13ec51f60db34f80
vagnerNatividade <vagnerflorindojunior@gmail.com>
2024-11-19 00:58:39 -0300

    set button color

--- components/header.tsx
+++ components/header.tsx
@@ -27,7 +27,7 @@ const Header = () => {
       </nav>
 
       <button
-        className={`w-52 py-2 rounded-sm bg-green-500 hover:bg-green-400 transition-all`}
+        className={`w-52 py-2 rounded-sm bg-button hover:bg-green-400 transition-all`}
       >
         Fale conosco
       </button>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        button: "#0DC74F",
       },
     },
   },
