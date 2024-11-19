c7e7d7c55fea48d78229c4a2e265442581936093
vagnerNatividade <vagnerflorindojunior@gmail.com>
2024-11-19 05:54:08 -0300

    create a contente in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
+import Button from "@/components/button";
 import Header from "@/components/header";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <h1>Hello World</h1>
+      <main className="mt-40 px-8">
+        <div className="w-[800px]">
+          <h1 className="text-7xl font-bold">
+            Melhor agência de marketing do bairro
+          </h1>
+          <p className="my-8 text-3xl opacity-85 ">
+            Somos uma agência de performance digital, aceleramos vendas e
+            aquisição de leads para grandes marcas.
+          </p>
+          <div className="w-max rounded-sm bg-white text-primary font-bold">
+            <Button title="Aumentar vendas" />
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
